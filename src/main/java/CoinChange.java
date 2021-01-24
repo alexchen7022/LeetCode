776c3654c9381a849a58bb801a9eb8f2b544f679
alexchen <alexchen7022@gmail.com>
2021-01-24 23:41:20 +0800

    Version 1

--- src/main/java/CoinChange.java
+++ src/main/java/CoinChange.java
@@ -2,61 +2,23 @@ import java.util.*;
 
 public class CoinChange {
 
-    private static ArrayList<Integer> coinType = new ArrayList<>();
-
-    private static Set<List<Integer>> mems = new HashSet<List<Integer>>();
-
     public static long change(int n, List<Long> c) {
+        Collections.sort(c);
+        int[] coins = new int[c.size()];
         for (int i = 0; i < c.size(); i++) {
             if (c.get(i) > Integer.MAX_VALUE) {
                 continue;
             }
-            coinType.add(c.get(i).intValue());
+            coins[i] = c.get(i).intValue();
         }
-        Collections.sort(coinType);
-        for (int totalValue = 1; totalValue <= n; totalValue++) {
-            Set<List<Integer>> tempMems = new HashSet<List<Integer>>();
-            for (List<Integer> existedList : mems) {
-                int remainValue = totalValue;
-                for (Integer existedCoin : existedList) {
-                    remainValue -= existedCoin;
-                }
-                for (int coinIndex = 0; coinIndex < coinType.size(); coinIndex++) {
-                    int coinValue = coinType.get(coinIndex);
-                    if (remainValue - coinValue == 0) {
-                        List<Integer> newList = new ArrayList<>(existedList);
-                        newList.add(coinValue);
-                        Collections.sort(newList);
-                        tempMems.add(newList);
-                    }
-                }
-            }
-            mems.addAll(tempMems);
-            for (int coinIndex = 0; coinIndex < coinType.size(); coinIndex++) {
-                int coinValue = coinType.get(coinIndex);
-                if (totalValue - coinValue >= 0) {
-                    List<Integer> newList = new ArrayList<>();
-                    newList.add(coinValue);
-                    mems.add(newList);
-                }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
             }
         }
-        int count = 0;
-        for (List<Integer> existedList : mems) {
-            int remainValue = n;
-            for (Integer existedCoin : existedList) {
-                remainValue -= existedCoin;
-            }
-            if (remainValue == 0) {
-                count++;
-            }
-        }
-        return count;
+        return dp[n];
     }
 
-
-    public static void init() {
-        coinType = new ArrayList<>();
-        mems = new HashSet<List<Integer>>();
-    }
 }
--- src/test/java/CoinChangeTest.java
+++ src/test/java/CoinChangeTest.java
@@ -12,10 +12,6 @@ public class CoinChangeTest {
     private List<Long> c;
     private int n;
 
-    @BeforeEach
-    void setUp() {
-        CoinChange.init();
-    }
 
     @Test
     void case1() {
